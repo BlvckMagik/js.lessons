2da78ca6dff2be06dc22d1b9117ed6585ff1147e
BlvckMagik <dandol777@gmail.com>
2021-06-21 14:55:46 +0300

    jest

---
+++ javascript-full/L13/4/calculator.test.js
@@ -0,0 +1,17 @@
+import { calc } from './calculator';
+
+it('should return summation result', () => {
+  expect(calc('1 + 2')).toEqual('1 + 2 = 3');
+});
+
+it('should return subtraction result', () => {
+  expect(calc('1 - 2')).toEqual('1 - 2 = -1');
+});
+
+it('should return multiplication result', () => {
+  expect(calc('1 * 2')).toEqual('1 * 2 = 2');
+});
+
+it('should return division result', () => {
+  expect(calc('2 / 1')).toEqual('2 / 1 = 2');
+});
