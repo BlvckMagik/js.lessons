be82251bec9b3615def657e863ac96d07c797136
BlvckMagik <dandol777@gmail.com>
2021-07-13 01:23:05 +0300

    les

--- javascript-full/L21/4/index.js
+++ javascript-full/L21/4/index.js
@@ -1,32 +1,11 @@
-class Order {
-  constructor(price, city, type) {
-    this.price = price;
-    this.city = city;
-    this.type = type;
-  }
+const getTitle = () => document.querySelector('.title').textContent;
+console.log(getTitle());
 
-  id = (Math.random().toFixed(4) * 10000).toString();
+const getDescription = () => document.querySelector('.about').innerText;
+console.log(getDescription());
 
-  isConfirmed = false;
+const getPlans = () => document.querySelector('.plans').innerHTML;
+console.log(getPlans());
 
-  dateCreated = new Date();
-
-  checkPrice() {
-    if (this.price > 1000) return true;
-    return false;
-  }
-
-  confirmOrder() {
-    this.isConfirmed = true;
-    this.dateConfirmed = new Date();
-  }
-
-  isValidType() {
-    if (this.type === 'Buy' || this.type === 'Sell') return true;
-    return false;
-  }
-}
-
-const order1 = new Order(100, 'Kyiv', 'Ka');
-const order2 = new Order(1000, 'Kyiv', 'Buy');
-const order3 = new Order(1001, 'Kyiv', 'Sell');
+const getGoal = () => document.querySelector('.goal').outerHTML;
+console.log(getGoal());
