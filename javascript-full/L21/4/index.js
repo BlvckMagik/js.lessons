775e7aba6ac2bcda29c9c0187bdfbcbc2ec047b0
BlvckMagik <dandol777@gmail.com>
2021-07-13 01:26:07 +0300

    les

--- javascript-full/L21/4/index.js
+++ javascript-full/L21/4/index.js
@@ -1,11 +1,19 @@
-export const getTitle = () => document.querySelector('.title').textContent;
+export function getTitle() {
+  return document.querySelector('.title').textContent;
+}
 console.log(getTitle());
 
-export const getDescription = () => document.querySelector('.about').innerText;
+export function getDescription() {
+  return document.querySelector('.about').innerText;
+}
 console.log(getDescription());
 
-export const getPlans = () => document.querySelector('.plans').innerHTML;
+export function getPlans() {
+  return document.querySelector('.plans').innerHTML;
+}
 console.log(getPlans());
 
-export const getGoal = () => document.querySelector('.goal').outerHTML;
+export function getGoal() {
+  return document.querySelector('.goal').outerHTML;
+}
 console.log(getGoal());
