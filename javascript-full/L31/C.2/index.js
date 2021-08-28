96810e88d64ef5ca23299f4fa8ed686eab92ffec
BlvckMagik <dandol777@gmail.com>
2021-08-28 11:31:40 +0300

    less

--- javascript-full/L31/C.2/index.js
+++ javascript-full/L31/C.2/index.js
@@ -4,14 +4,14 @@ const asyncCalculator = numbr =>
       console.log(`Initial value: ${numbr}`);
       resolve(numbr);
     }, 500);
-  })
-    .then(num => {
+  }).then(num =>
+    new Promise(resolve => {
       setTimeout(() => {
         const res = num * num;
         console.log(`Squared value: ${res}`);
         resolve(res);
       }, 500);
-    })
-    .then(num => console.log(`Doubled value: ${num * 2}`));
+    }).then(num => console.log(`Doubled value: ${num * 2}`))
+  );
 
 asyncCalculator(3);
--- javascript-full/L31/C.4/index.js
+++
@@ -1,7 +0,0 @@
-const failedPromise = new Promise((resolve, reject) => {
-  reject(new Error('Oops, error!'));
-});
-
-failedPromise.catch(function onError(error) {
-  console.log(error);
-});
